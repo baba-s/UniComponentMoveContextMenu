434711efcc2fe2f090d45ad0e3d2e6b24d46c5c8
baba_s <shotababa1206@gmail.com>
2020-06-12 22:50:31 +0900

    first commit

---
+++ Editor/ComponentMoveContextMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	internal static class ComponentMoveContextMenu
+	{
+		[MenuItem( "CONTEXT/Component/Move Top" )]
+		private static void MoveTop( MenuCommand command )
+		{
+			var component = ( Component ) command.context;
+			while ( ComponentUtility.MoveComponentUp( component ) )
+			{
+			}
+		}
+	}
+}
